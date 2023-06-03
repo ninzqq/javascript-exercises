2b4a3aca322ebdd82b48d2a5dea8a2a402c3e1f8
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-03 14:07:24 +0300

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let repeatedString = "";
+    if (times < 0) {
+        return repeatedString = "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
