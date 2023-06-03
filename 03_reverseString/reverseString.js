e1ecc4501f9b95a4a0fb6e2744c94d85442f1a8c
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-03 23:54:52 +0300

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splittedStr = str.split('');
+    let reversedStr = '';
 
+    for (let i = splittedStr.length - 1; i >= 0; i--) {
+        reversedStr = reversedStr.concat(splittedStr[i]);
+    }
+
+    return reversedStr;
 };
 
 // Do not edit below this line
