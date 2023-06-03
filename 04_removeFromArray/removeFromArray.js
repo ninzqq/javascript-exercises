001cc87f05323374cb253939cc10f18fa25dba27
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-04 02:59:17 +0300

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (list) {
+  const args = Array.prototype.slice.call(arguments, 1);
 
+  if (Array.isArray(list)) {
+    for (let i = 0; i <= args.length - 1; i++) {
+      let idx = list.indexOf(args[i]);
+      if (list[idx] === args[i]) {
+        list.splice(idx, 1);
+      }
+    }
+  }
+
+  return list;
 };
 
 // Do not edit below this line
