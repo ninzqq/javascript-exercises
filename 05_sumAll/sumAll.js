6b318b7ba2405ca3bff08bd58c048eebb9fa2d58
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-04 03:23:37 +0300

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+  }
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
 
+  let total = (end * (end + 1)) / 2;
+  let startPortion = ((start - 1) * (start - 1 + 1)) / 2;
+  return total - startPortion;
 };
 
 // Do not edit below this line
