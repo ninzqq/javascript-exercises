fff8c985d1b8a6d749a7ac1e71786fd99f2c58d8
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-04 04:05:39 +0300

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let rounded = Math.round((value - 32) * (5 / 9) * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let rounded = Math.round(((value * (9 / 5)) + 32) * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
